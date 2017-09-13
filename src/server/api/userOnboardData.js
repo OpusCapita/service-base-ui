03a27f16ede38f2e84046496ad6a1c71fc158c60
Christian Drengenberg <ChristianDrengenberg@googlemail.com>
2017-09-13 14:38:02 +0200

    Fixed notNull Violation bug.

--- src/server/api/userOnboardData.js
+++ src/server/api/userOnboardData.js
@@ -52,8 +52,7 @@ module.exports.find = function(search)
 module.exports.updateByInvitationCode = function(invitationCode, userId, data)
 {
     delete data.invitationCode;
-    delete data.userId;
-    
+
     data = serializeSubProperties(data.dataValues || data);
 
     const where = {
