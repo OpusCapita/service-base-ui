681dae344607fdf5fdcf406e6a7b8d395705bc91
levchuk <anton.levchuk@jcatalog.com>
2017-10-11 12:47:12 +0300

    #65 Added demo user for invoice matching.

---
+++ src/server/db/migrations/017-userRoles.test.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const roles = require('../data/userRoles-17.json');
+
+/**
+ * Inserts test data into existing database structures.
+ * If all migrations were successul, this method will never be executed again.
+ * To identify which migrations have successfully been processed, a migration's filename is used.
+ *
+ * @param {object} data - [Sequelize]{@link https://github.com/sequelize/sequelize} instance.
+ * @param {object} config - A model property for database models and everything from [config.data]{@link https://github.com/OpusCapitaBusinessNetwork/db-init} passed when running the db-initialization.
+ * @returns {Promise} [Promise]{@link http://bluebirdjs.com/docs/api-reference.html}
+ * @see [Applying data migrations]{@link https://github.com/OpusCapitaBusinessNetwork/db-init#applying-data-migrations}
+ */
+module.exports.up = function(db, config)
+{
+    roles.forEach(role => role.createdOn = new Date());
+
+    return db.queryInterface.bulkInsert('UserRole', roles);
+}
+
+/**
+ * Reverts all migrations for databse tables and data.
+ * If the migration process throws an error, this method is called in order to revert all changes made by the up() method.
+ *
+ * @param {object} data - [Sequelize]{@link https://github.com/sequelize/sequelize} instance.
+ * @param {object} config - A model property for database models and everything from [config.data]{@link https://github.com/OpusCapitaBusinessNetwork/db-init} passed when running the db-initialization.
+ * @returns {Promise} [Promise]{@link http://bluebirdjs.com/docs/api-reference.html}
+ * @see [Applying data migrations]{@link https://github.com/OpusCapitaBusinessNetwork/db-init#applying-data-migrations}
+ */
+module.exports.down = function(db, config)
+{
+    return db.queryInterface.bulkDelete('UserRole', {
+        id : {
+            $in : roles.map(role => role.id)
+        }
+    });
+}
