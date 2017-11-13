d6fddde0dd67a88995bc512fae08ecdd049d515a
Karol Maciaszek <karol@maciaszek.pl>
2017-11-13 09:52:28 +0100

    Moved userData mock to nginx-rp

--- local/index.js
+++ local/index.js
@@ -3,15 +3,6 @@
 const db = require('ocbesbn-db-init');
 const server = require('ocbesbn-web-init');
 
-const mockUserData = {
-	id: 'john.doe@ncc.com'
-};
-
-const mockUserDataMiddleware = (req, res, next) => {
-	req.opuscapita.userData = key => mockUserData[key];
-	next();
-};
-
 db.init({ consul : { host : 'consul' }, retryCount: 50 })
 	.then(db => server.init({
 		routes : { dbInstance : db },
@@ -22,8 +13,7 @@ db.init({ consul : { host : 'consul' }, retryCount: 50 })
 			webpack: {
 				useWebpack: true,
 				configFilePath: __dirname + '/../webpack.development.config.js'
-			},
-			middlewares: [mockUserDataMiddleware]
+			}
 		},
 		serviceClient : {
 			injectIntoRequest : true,
