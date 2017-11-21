e46fa73d391ea72d8f22172a7de1f1b9e131cdad
Christian Drengenberg <ChristianDrengenberg@googlemail.com>
2017-11-21 13:16:37 +0100

    Webpack enhancement.

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,6 +1,7 @@
 const baseConfig = require('./dev/webpack.config');
 const extend = require('extend');
 const webpack = require('webpack');
+const Visualizer = require('webpack-visualizer-plugin');
 
 delete baseConfig.entry.main;
 delete baseConfig.output.publicPath;
@@ -14,31 +15,38 @@ const config = {
             root: 'React',
             commonjs2: 'react',
             commonjs: 'react',
-            umd: 'react'
+            amd: 'react'
         },
         'react-dom': {
             root: 'ReactDOM',
             commonjs2: 'react-dom',
             commonjs: 'react-dom',
-            umd: 'react-dom'
-        }
+            amd: 'react-dom'
+        },
+        'react-router': {
+            root: 'ReactRouter',
+            commonjs2: 'react-router',
+            commonjs: 'react-router',
+            amd: 'react-router'
+        },
+        jquery : 'jquery',
+        moment : 'moment'
     },
     plugins: [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': '"production"'
         }),
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            jQuery: "jquery",
-            jquery:"jquery"
-        }),
         new webpack.optimize.OccurrenceOrderPlugin(true),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false,
-                screw_ie8: true
+                screw_ie8: true,
+                drop_console : true
             },
             comments: false
+        }),
+        new Visualizer({
+          filename: './statistics.html'
         })
     ]
 }
