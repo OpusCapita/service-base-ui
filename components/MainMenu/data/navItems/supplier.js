916faf5a270879119bf3ef505176582f1252f440
nczeranka <norbert.czeranka@opuscapita.com>
2018-10-05 15:37:17 +0000

    added swedish and finnish menu items

--- components/MainMenu/data/navItems/supplier.js
+++ components/MainMenu/data/navItems/supplier.js
@@ -173,5 +173,179 @@ export default
             "label": "Routen",
             "link": "/bnp/routing/routes"
         }]
+    }],
+
+    "sv": [
+    {
+        "key": "home",
+        "label": "Hem",
+        "link": "/bnp"
+    },
+    {
+        "key": "orders",
+        "label": "Ordrar",
+        "children": [
+        {
+            "key": "overview",
+            "label": "Översikt",
+            "link": "/sales-order"
+        },
+        {
+            "key": "orderConfiguration",
+            "label": "Inställningar för mottag av order",
+            "link": "/sales-order/configuration"
+        },
+        {
+            "key": "customerConnections",
+            "label": "Kundanslutningar",
+            "link": "/sales-order/customer-connections"
+        }]
+    },
+    {
+        "key": "invoices",
+        "label": "Fakturor",
+        "children": [
+        {
+            "key": "overview",
+            "label": "Översikt",
+            "link": "/sales-invoice/search"
+        },
+        {
+            "key": "key-in",
+            "label": "Fakturaportal",
+            "link": "/sales-invoice/create"
+        },
+        {
+            "key": "configuration",
+            "label": "Inställningar för att skicka Fakturor",
+            "link": "/einvoice-send/configuration"
+        },
+        {
+            "key": "customer-connections",
+            "label": "Kundanslutningar",
+            "link": "/einvoice-send/customer-connections"
+        }]
+    },
+    {
+        "key": "catalog",
+        "label": "Kataloger",
+        "children": [
+        {
+            "key": "upload",
+            "label": "Ladda upp",
+            "link": "/bnp/catalog-upload"
+        }]
+    },
+    {
+        "key": "company",
+        "label": "Företag",
+        "children": [
+        {
+            "key": "profile",
+            "label": "Profil",
+            "link": "/bnp/supplierInformation"
+        },
+        {
+            "key": "users",
+            "label": "Användare",
+            "link": "/bnp/users"
+        }]
+    },
+    {
+        "key": "settings",
+        "label": "Inställningar",
+        "children": [
+        {
+            "key": "routes",
+            "label": "Anslutningar",
+            "link": "/bnp/routing/routes"
+        }]
+    }],
+
+    "fi": [
+    {
+        "key": "home",
+        "label": "Koti",
+        "link": "/bnp"
+    },
+    {
+        "key": "orders",
+        "label": "Tilaukset",
+        "children": [
+        {
+            "key": "overview",
+            "label": "Yhteenveto",
+            "link": "/sales-order"
+        },
+        {
+            "key": "orderConfiguration",
+            "label": "Vastaanotto konfiguraatio",
+            "link": "/sales-order/configuration"
+        },
+        {
+            "key": "customerConnections",
+            "label": "Asiakasyhteydet",
+            "link": "/sales-order/customer-connections"
+        }]
+    },
+    {
+        "key": "invoices",
+        "label": "Laskut",
+        "children": [
+        {
+            "key": "overview",
+            "label": "Yhteenveto",
+            "link": "/sales-invoice/search"
+        },
+        {
+            "key": "key-in",
+            "label": "Laskuportaali",
+            "link": "/sales-invoice/create"
+        },
+        {
+            "key": "configuration",
+            "label": "Lähetys konfiguraatio",
+            "link": "/einvoice-send/configuration"
+        },
+        {
+            "key": "customer-connections",
+            "label": "Asiakasyhteydet",
+            "link": "/einvoice-send/customer-connections"
+        }]
+    },
+    {
+        "key": "catalog",
+        "label": "Katalogit",
+        "children": [
+        {
+            "key": "upload",
+            "label": "Lataa",
+            "link": "/bnp/catalog-upload"
+        }]
+    },
+    {
+        "key": "company",
+        "label": "Yritys",
+        "children": [
+        {
+            "key": "profile",
+            "label": "Profiili",
+            "link": "/bnp/supplierInformation"
+        },
+        {
+            "key": "users",
+            "label": "Käyttäjät",
+            "link": "/bnp/users"
+        }]
+    },
+    {
+        "key": "settings",
+        "label": "Asetukset",
+        "children": [
+        {
+            "key": "routes",
+            "label": "Reititys",
+            "link": "/bnp/routing/routes"
+        }]
     }]
 }
