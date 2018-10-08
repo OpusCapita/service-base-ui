bc3453415b195151f56e875903c2a039a961fffc
Christian Drengenberg <Christian.Drengenberg@opuscapita.com>
2018-10-08 11:37:44 +0200

    Fixed translations.

--- components/MainMenu/Menu/components/Notifications/i18n/en.js
+++
@@ -1,5 +0,0 @@
-export default {
-    'Menu.Notifications.newNotifications' : 'New notifications',
-    'Menu.Notifications.noNewNotifications' : 'There are no new notifications.',
-    'Menu.Notifications.markAllRead': 'Read all'
-}
