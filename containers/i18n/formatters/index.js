06b128c4daf2e3eb5a8eff2dc82ae81343f38c47
nczeranka <norbert.czeranka@opuscapita.com>
2019-04-16 14:53:52 +0000

    added formatter for es, fr, it and pt

--- containers/i18n/formatters/index.js
+++ containers/i18n/formatters/index.js
@@ -1,6 +1,10 @@
-import en from './en';
 import de from './de';
+import en from './en';
+import es from './es';
 import fi from './fi';
+import fr from './fr';
+import it from './it';
+import pt from './pt';
 import sv from './sv';
 
-export default { en, de, fi, sv };
+export default { de, en, es, fi, fr, it, pt, sv };
