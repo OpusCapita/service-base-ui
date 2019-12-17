cc7565e3e1ebb24f417328d79ee27656191fcf9a
Christian Drengenberg <Christian.Drengenberg@opuscapita.com>
2019-12-17 17:16:03 +0100

    Added Spanish translations.

--- containers/i18n/system/index.js
+++ containers/i18n/system/index.js
@@ -1,6 +1,7 @@
 const de = require('./de.json');
+const es = require('./es.json');
 const en = require('./en.json');
 const sv = require('./sv.json');
 const fi = require('./fi.json');
 
-module.exports = { de, en, sv, fi };
+module.exports = { de, es, en, sv, fi };
