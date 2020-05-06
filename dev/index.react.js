79e63cb25e979538569dce4c4bd48adabc91c4e6
Igor Dudschenko <igor.dudschenko@cdi-ag.de>
2020-05-06 15:41:47 +0200

    BPR-4006: Added full-width styling for ModalDialogs

--- dev/index.react.js
+++ dev/index.react.js
@@ -27,6 +27,7 @@ class Test extends ContextComponent
                 <ModalDialog
                     onClose={() => this.setState()}
                     ref={ref => this.modal = ref}
+                    size='full-width'
                 />
                 <button onClick={() => this.modal.show()}>Open modal</button>
             </div>
