da8e795b8fe9cb683281c6ae5c7df7bf0a86e17d
Igor Dudschenko <igor.dudschenko@cdi-ag.de>
2020-06-22 10:57:42 +0200

    BPR-4202: Added polish translations

--- containers/i18n/system/index.js
+++ containers/i18n/system/index.js
@@ -3,5 +3,6 @@ const es = require('./es.json');
 const en = require('./en.json');
 const sv = require('./sv.json');
 const fi = require('./fi.json');
+const pl = require('./pl.json');
 
-module.exports = { de, es, en, sv, fi };
+module.exports = { de, es, en, sv, fi, pl };
