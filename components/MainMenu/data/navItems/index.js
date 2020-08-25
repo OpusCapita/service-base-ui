bacc6dbcf4c4e91290c329f95a1aae9070b20070
Tore Pettersen <toreskog@live.com>
2020-08-25 13:13:33 +0000

    Remove old menues

--- components/MainMenu/data/navItems/index.js
+++ components/MainMenu/data/navItems/index.js
@@ -1,7 +1,5 @@
-import customer from './customer';
-import supplier from './supplier';
 import businessPartner from './businessPartner';
 import admin from './admin';
 
-export { customer, supplier, admin, businessPartner };
-export default { customer, supplier, admin, businessPartner };
+export { admin, businessPartner };
+export default { admin, businessPartner };
