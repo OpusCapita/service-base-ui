a6a78726bf31df822b42b8b3269f46295f815913
ElaCzapiewska <elzbieta.rymarkiewicz-czapiewska@opuscapita.com>
2021-01-20 09:02:31 +0100

    feature/GENESIS-306-default-lang-selection-english (#92)

--- components/MainMenu/Menu/index.js
+++ components/MainMenu/Menu/index.js
@@ -10,6 +10,7 @@ import MenuSearch from './components/MenuSearch';
 import MenuSelect from './components/MenuSelect';
 import NavigationBar from './components/NavigationBar';
 import Notifications from './components/Notifications';
+import MenuSelectLanguage from './components/MenuSelectLanguage';
 
-export { Menu, MenuAccount, MenuAccountIcon, MenuDropdownGrid, MenuDropdownList, MenuIcon, MenuIconsBar, MenuLogo, MenuSearch, MenuSelect, NavigationBar, Notifications };
-export default { Menu, MenuAccount, MenuAccountIcon, MenuDropdownGrid, MenuDropdownList, MenuIcon, MenuIconsBar, MenuLogo, MenuSearch, MenuSelect, NavigationBar, Notifications };
+export { Menu, MenuAccount, MenuAccountIcon, MenuDropdownGrid, MenuDropdownList, MenuIcon, MenuIconsBar, MenuLogo, MenuSearch, MenuSelect, NavigationBar, Notifications, MenuSelectLanguage };
+export default { Menu, MenuAccount, MenuAccountIcon, MenuDropdownGrid, MenuDropdownList, MenuIcon, MenuIconsBar, MenuLogo, MenuSearch, MenuSelect, NavigationBar, Notifications, MenuSelectLanguage };
